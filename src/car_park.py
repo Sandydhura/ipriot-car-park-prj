99a921c7cb8d9d673cf6741da7f2aa3a5eaf10d9
20148938 <20148938@tafe.wa.edu.au>
2025-06-27 10:45:41 +0800

    Added unit tests for logging car activity

--- src/car_park.py
+++ src/car_park.py
@@ -1,14 +1,18 @@
 from display import Display
 from sensor import Sensor
-
+from pathlib import Path
+from datetime import datetime
 
 class CarPark:
-    def __init__(self, location="Unknown", capacity=100, plates=None, displays=None):
+    def __init__(self, location="Unknown", capacity=100, plates=None, displays=None, log_file=Path("log.txt")):
         self.sensors = None
         self.location = location
         self.capacity = capacity
         self.plates = plates or []         # List of car plate numbers
         self.displays = displays or []     # List of Display objects
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        # create the file if it doesn't exist:
+        self.log_file.touch(exist_ok=True)
 
 
     def __str__(self):
@@ -40,11 +44,17 @@ class CarPark:
         else:
             self.plates.append(plate)
             self.update_displays()
+            self._log_car_activity(plate, "entered")
 
     def remove_car(self, plate):
         if plate in self.plates:
             self.plates.remove(plate)
             self.update_displays()
+            self._log_car_activity(plate, "entered")
         else:
             raise ValueError(f"Car {plate} not found.")
+
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as f:
+            f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
 
